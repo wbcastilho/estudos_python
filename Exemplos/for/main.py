fd89b8952d49dd26f2fb002c59c5dbc4fa934ab7
welbercastilho <welber.pereira@eptv.com.br>
2021-11-05 13:09:59 -0300

    Alterado exemplos tuplas

--- Exemplos/for/main.py
+++ Exemplos/for/main.py
@@ -1,19 +1,18 @@
 # Loops / laços f
-
-'''
+"""
     for variavel_temporaria in exemplo_array:
         fazer_algo_aqui_dentro
-'''
+"""
 
 # Quando o programa descobre a quantidade por você
-'''fotos = ['foto1', 'foto2', 'foto3', 'foto4', 'foto5']
+fotos = ['foto1', 'foto2', 'foto3', 'foto4', 'foto5']
 for foto in fotos:
-    print(foto)'''
+    print(foto)
 
 # Você ja sabe a quantidade de vezes que deve rodar
 # Função range roda até o penúltimo número
-'''for volume in range(11):
-    print(f'Aumentando o volume para {volume}')'''
+for volume in range(11):
+    print(f'Aumentando o volume para {volume}')
 
 # Passando valor inicial e final para o range
 for volume in range(1, 11):
--- Exemplos/tuplas/main.py
+++ Exemplos/tuplas/main.py
@@ -1,9 +1,35 @@
 # Tuplas - Não se pode alterar ou remover intens de uma tupla, ou seja, são valores imutáveis
 
+# Declarando tuplas
 sites = ('youtube.com', 'facebook.com', 'instagran.com')
-valores = (23,34,65)
+valores = (23, 34, 65)
 print(sites[2])
 print(valores[0])
 
+# Declarando tuplas
+nome_da_tupla = (1, "olá", 1.5)    # tupla heterogênea
+print(nome_da_tupla)
+
+# Declarando tuplas
 cores_tamanhos = ('vermelho', 'verde', 'azul') + ('alto', 'baixo', 'médio')
 print(cores_tamanhos)
+
+# desempacotamento de tuplas
+x, y = (9, 10)
+print(x)
+print(y)
+
+# retornando tuplas em funções
+def maior(value):
+    ma = value[0]
+    pos = 0
+    for i in range(1, len(value)):
+        if value[i] > ma:
+            ma = value[i]
+            pos = i
+    return ma, pos
+
+
+x, y = maior([1, 2, 3])
+print(x)
+print(y)
