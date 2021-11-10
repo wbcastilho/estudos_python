5e92148a1663bfdb50a5e6d65e7791c2f7919993
welbercastilho <welber.pereira@eptv.com.br>
2021-11-10 19:42:41 -0300

    Alterado enumerate

--- Exemplos/enumerate/main.py
+++ Exemplos/enumerate/main.py
@@ -8,4 +8,9 @@
     1 banana
     2 grapes
     3 pear
+
+    Enumera as letras da palavra, começando em 1
+    >>> list(enumerate('albatroz', 1))
+    [(1, 'a'), (2, 'l'), (3, 'b'), (4, 'a'), (5, 't'), (6, 'r'), (7, 'o'), (8, 'z')]
+
 """
